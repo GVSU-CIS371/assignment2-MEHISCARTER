7608b8cb99683efd3e630cbd465aeb9a5abfe884
carter <carterpylman@gmail.com>
2025-10-20 21:23:43 -0400

    Added radio buttons but no functionality yet

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,60 @@
 <template>
+  <Beverage :isIced="currentTemp === 'Cold'" />
+  <div class="base-options">
+    <ul>
+      <li>
+        <template v-for="base in bases" :key="base.id">
+          <label>
+            <input
+              type="radio"
+              name="base_selection"
+              :id="`r${base.name}`"
+              :value="base.name"
+              v-model="selectedBase"
+            />
+            {{ base.name }}
+          </label>
+        </template>
+      </li>
+    </ul>
+  </div>
   <div>
-    <Beverage :isIced="currentTemp === 'Cold'" />
+    <ul>
+      <li>
+        <template v-for="cream in creamers" :key="cream.id">
+          <label>
+            <input
+              type="radio"
+              name="cream_selection"
+              :id="`r${cream.name}`"
+              :value="cream.name"
+              v-model="selectedCreamer"
+            />
+            {{ cream.name }}
+          </label>
+        </template>
+      </li>
+    </ul>
+  </div>
+  <div>
+    <ul>
+      <li>
+        <template v-for="syrup in syrups" :key="syrup.id">
+          <label>
+            <input
+              type="radio"
+              name="syrup_selection"
+              :id="`r${syrup.name}`"
+              :value="syrup.name"
+              v-model="selectedSyrup"
+            />
+            {{ syrup.name }}
+          </label>
+        </template>
+      </li>
+    </ul>
+  </div>
+  <div>
     <ul>
       <li>
         <template v-for="temp in temps" :key="temp">
@@ -21,8 +75,15 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import Beverage from "./components/Beverage.vue";
 import { temps, currentTemp } from "./stores/beverage";
+import { bases } from "./stores/beverage";
+import { creamers } from "./stores/beverage";
+import { syrups } from "./stores/beverage";
+const selectedBase = ref("");
+const selectedCreamer = ref("");
+const selectedSyrup = ref("");
 </script>
 
 <style lang="scss">
